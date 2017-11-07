6ed7d63751d601c6c8dd9eba5b94ee6272553cbb
kwlee <keith.won.lee@gmail.com>
2017-11-07 14:25:00 -0800

    completed survey form

---
+++ routes/index.js
@@ -0,0 +1,16 @@
+module.exports = function Route(app){
+	// root route to render the index.ejs view
+	app.get('/', function(req, res) {
+	 res.render("index");
+	});
+	// post route for adding a data from a survey
+	app.post('/result', function(req, res) {
+		const submittedInfo = {
+			name: req.body.name,
+			dojoLocation: req.body.dojo,
+			favoriteLanguage: req.body.language,
+			comment: req.body.comment
+    };
+	 	res.render("result", { userData: submittedInfo });
+	});
+};
